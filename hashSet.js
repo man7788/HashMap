f9da66415e8b62fd65388f58474c21f2dc1a0dd5
man7788 <jerman1841@gmail.com>
2024-08-01 10:48:27 +0800

    Add checkbound, set, length methods

--- hashSet.js
+++ hashSet.js
@@ -1,3 +1,5 @@
+import LinkedList from './linkedList.js';
+
 class HashSet {
   constructor(loadFactor) {
     this.loadFactor = loadFactor;
@@ -6,6 +8,12 @@ class HashSet {
   capacity = 16;
 
   buckets = new Array(this.capacity);
+
+  checkBound(index) {
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error('Trying to access index out of bound');
+    }
+  }
 
   getSet(index) {
     let returnValue = null;
@@ -35,6 +43,63 @@ class HashSet {
       }
       return hashCode;
     }
+  }
+
+  set(key) {
+    const setKey = (keyValue) => {
+      const index = this.hash(keyValue);
+
+      this.checkBound(index);
+
+      if (this.buckets[index] === undefined) {
+        this.buckets[index] = LinkedList();
+        this.buckets[index].append(keyValue);
+      } else {
+        const keyIndex = this.buckets[index].find(keyValue);
+        if (keyIndex !== null) {
+          this.buckets[index].insertAt(keyValue, keyIndex);
+          this.buckets[index].removeAt(keyIndex + 1);
+        } else {
+          this.buckets[index].append(keyValue);
+        }
+      }
+    };
+
+    const checkLoadFactor = () => {
+      const threshold = this.loadFactor * this.capacity;
+      const entries = this.length();
+
+      if (entries + 1 > Math.floor(threshold)) {
+        this.capacity *= 2;
+        const newBuckets = new Array(this.capacity);
+        const oldBuckets = this.buckets;
+        this.buckets = newBuckets;
+        // Iterate ALL keys (all linked list elements)
+        for (let i = 0; i < oldBuckets.length; i++) {
+          if (oldBuckets[i] !== undefined) {
+            const entriesList = oldBuckets[i].toEntries();
+            entriesList.forEach((element) => {
+              // Store each key to bucket with new hash key
+              this.setKey(element[0]);
+            });
+          }
+        }
+      }
+    };
+
+    checkLoadFactor();
+    setKey(key);
+  }
+
+  length() {
+    let returnLength = 0;
+    for (let i = 0; i < this.buckets.length; i++) {
+      const element = this.buckets[i];
+      if (element !== undefined) {
+        returnLength += this.buckets[i].size();
+      }
+    }
+    return returnLength;
   }
 }
 
